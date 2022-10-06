7ccb72829890bc7c022e6aa91f511c635364f8ba
adeirfanimanda <adeheaven09@gmail.com>
2022-10-06 10:43:11 +0700

    Plankton berhasil mencuri krabby patty

---
+++ Resep.java
@@ -0,0 +1,11 @@
+/**
+ * Resep
+ */
+public class Resep {
+    public static void main(String[] args) {
+        System.out.println("Resep Krabby Patty");
+        System.out.println("======================");
+        System.out.println("1. Bawang");
+        System.out.println("2. Ketimun");
+    }
+}
